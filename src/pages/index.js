55e274f112ffb3865f942c3577dac8618e8eab24
Swish27 <73870887+Swish27@users.noreply.github.com>
2022-11-13 21:37:51 -0700

    Personalized index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,22 +9,16 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className='title'>Congratulations! You've set up your webapp!</h1>
+        <h1 className='title'>Welcome to our group's website!</h1>
 
         <p className='description'>
-          Get started by editing <code>src/pages/index.js</code> to be your
-          project home page.
+          Made by Jake Carroll, Justin Goh, and Tanmay Meti
         </p>
-        <p className='description2'>
-          Next, each team member will create their own about me file in the
-          pages directory:{' '}
-        </p>
-        <code>src/page/teamMemberName.js</code>
 
         <div className='grid'>
-          <a href='/ronBurgundy' className='card'>
-            <h3>Example About Me Page &rarr;</h3>
-            <p>Click here to navigate to an example page</p>
+          <a href='/jakeCarroll' className='card'>
+            <h3>Jake Carroll's Page</h3>
+            <p>Click here to navigate to an Jake's page</p>
           </a>
           <a
             href='https://github.com/wangalec/website-template'
@@ -70,7 +64,6 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
-
         main {
           padding: 5rem 0;
           flex: 1;
@@ -79,7 +72,6 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
-
         footer {
           width: 100%;
           height: 100px;
@@ -88,54 +80,44 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
-
         footer img {
           margin-left: 0.5rem;
         }
-
         footer a {
           display: flex;
           justify-content: center;
           align-items: center;
         }
-
         a {
           color: inherit;
           text-decoration: none;
         }
-
         .title a {
           color: #0070f3;
           text-decoration: none;
         }
-
         .title a:hover,
         .title a:focus,
         .title a:active {
           text-decoration: underline;
         }
-
         .title {
           margin: 0;
           line-height: 1.15;
           font-size: 4rem;
         }
-
         .title,
         .description {
           text-align: center;
         }
-
         .description {
           line-height: 1.5;
           font-size: 1.5rem;
         }
-
         .description2 {
           line-height: 1.5;
           font-size: 1.25rem;
         }
-
         code {
           background: #fafafa;
           border-radius: 5px;
@@ -144,17 +126,14 @@ export default function Home() {
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
-
         .grid {
           display: flex;
           align-items: center;
           justify-content: center;
           flex-wrap: wrap;
-
           max-width: 800px;
           margin-top: 3rem;
         }
-
         .card {
           margin: 1rem;
           flex-basis: 45%;
@@ -166,29 +145,24 @@ export default function Home() {
           border-radius: 10px;
           transition: color 0.15s ease, border-color 0.15s ease;
         }
-
         .card:hover,
         .card:focus,
         .card:active {
           color: #0070f3;
           border-color: #0070f3;
         }
-
         .card h3 {
           margin: 0 0 1rem 0;
           font-size: 1.5rem;
         }
-
         .card p {
           margin: 0;
           font-size: 1.25rem;
           line-height: 1.5;
         }
-
         .logo {
           height: 1em;
         }
-
         @media (max-width: 600px) {
           .grid {
             width: 100%;
@@ -206,7 +180,6 @@ export default function Home() {
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
         }
-
         * {
           box-sizing: border-box;
         }
