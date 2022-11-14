27bc89b7668ac9dbdb6e7691f3808bc08bac1d94
Swish27 <73870887+Swish27@users.noreply.github.com>
2022-11-13 21:22:01 -0700

    Back to original

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,9 +22,9 @@ export default function Home() {
         <code>src/page/teamMemberName.js</code>
 
         <div className='grid'>
-          <a href='/jakeCarroll' className='card'>
-            <h3>Jake Carroll's Page' &rarr;</h3>
-            <p>Click here to navigate to an Jake's page</p>
+          <a href='/ronBurgundy' className='card'>
+            <h3>Example About Me Page &rarr;</h3>
+            <p>Click here to navigate to an example page</p>
           </a>
           <a
             href='https://github.com/wangalec/website-template'
@@ -70,6 +70,7 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
+
         main {
           padding: 5rem 0;
           flex: 1;
@@ -78,6 +79,7 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
+
         footer {
           width: 100%;
           height: 100px;
@@ -86,44 +88,54 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
+
         footer img {
           margin-left: 0.5rem;
         }
+
         footer a {
           display: flex;
           justify-content: center;
           align-items: center;
         }
+
         a {
           color: inherit;
           text-decoration: none;
         }
+
         .title a {
           color: #0070f3;
           text-decoration: none;
         }
+
         .title a:hover,
         .title a:focus,
         .title a:active {
           text-decoration: underline;
         }
+
         .title {
           margin: 0;
           line-height: 1.15;
           font-size: 4rem;
         }
+
         .title,
         .description {
           text-align: center;
         }
+
         .description {
           line-height: 1.5;
           font-size: 1.5rem;
         }
+
         .description2 {
           line-height: 1.5;
           font-size: 1.25rem;
         }
+
         code {
           background: #fafafa;
           border-radius: 5px;
@@ -132,14 +144,17 @@ export default function Home() {
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
+
         .grid {
           display: flex;
           align-items: center;
           justify-content: center;
           flex-wrap: wrap;
+
           max-width: 800px;
           margin-top: 3rem;
         }
+
         .card {
           margin: 1rem;
           flex-basis: 45%;
@@ -151,24 +166,29 @@ export default function Home() {
           border-radius: 10px;
           transition: color 0.15s ease, border-color 0.15s ease;
         }
+
         .card:hover,
         .card:focus,
         .card:active {
           color: #0070f3;
           border-color: #0070f3;
         }
+
         .card h3 {
           margin: 0 0 1rem 0;
           font-size: 1.5rem;
         }
+
         .card p {
           margin: 0;
           font-size: 1.25rem;
           line-height: 1.5;
         }
+
         .logo {
           height: 1em;
         }
+
         @media (max-width: 600px) {
           .grid {
             width: 100%;
@@ -186,6 +206,7 @@ export default function Home() {
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
         }
+
         * {
           box-sizing: border-box;
         }
